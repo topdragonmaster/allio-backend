28fbfc6edfd80b6ea68e8a6f122ac8b4d3fa56a6
topdragonmaster <topdragonmaster@gmail.com>
2021-08-24 16:51:32 +0000

    BE-8 Add open api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,15 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter()
   );
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Main Backend')
+    .setDescription('Main Backend of Allio')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
